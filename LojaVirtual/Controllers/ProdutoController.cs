9bb2c94b6d4c125fa0676385b6d8d912ba98b302
João Rafael Fagundes Menezes <joaorafael652@outlook.com>
2019-08-23 02:14:12 -0300

    Adicionar arquivos de projeto.

---
+++ LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        //deve retornar iactionresult ou actionresult
+        public ActionResult Visualizar()
+        {
+            var produto = GetProduto();
+            //return new ContentResult() {Content = "<h3>Produto->Visualizar</h3>", ContentType = "text/html"};
+            return View(produto);
+        }
+
+        private Produto GetProduto()
+        {
+            return new Produto()
+            {
+                Id = 1,
+                Descricao = "Jogue em 4K",
+                Nome = "Xbox One X",
+                Valor = 2000.00M
+            };
+        }
+    }
+}
