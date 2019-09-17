76a853abc9620326d9a474fc4e5a8ac054f406ab
João Rafael Fagundes Menezes <menezes.jrafael@gmail.com>
2019-09-16 22:40:31 -0300

    Criação da classe modelo Categoria

---
+++ LojaVirtual/Models/Categoria.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        /*
+         * Nome: Telefone sem fio
+         * Slug: telefone-sem-fio
+         * URL normal: www.lojavirtual.com.br/categoria/5
+         * URL amigável e com Slug: www.lojavirtual.com.br/categoria/informatica
+         */
+        public string Slug  { get; set; }
+
+        /*
+         * Auto-relacionamento
+         * 1 - Informática - P : null
+         * - 2 - Mouse : P : 1
+         * -- 3 - Mouse sem fio : P : 2
+         * -- 4 - Mouse gamer : P : 2
+         */
+
+        public int? CategoriaPaiId { get; set; }
+
+        //ORM Entityframeworkcore
+        [ForeignKey("CategoriaPaiId")]
+        public virtual Categoria CategoriaID { get; set; }
+    }
+}
