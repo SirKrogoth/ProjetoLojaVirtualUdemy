a25e1199a74578a0a315f07fd1575d80a7d02a70
João Rafael Fagundes Menezes <menezes.jrafael@gmail.com>
2019-10-13 18:27:31 -0300

    Criação do Controller Colaborador e tambem alteração na classe de colaborador.

--- LojaVirtual/Models/Colaborador.cs
+++ LojaVirtual/Models/Colaborador.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using LojaVirtual.Libraries.Lang;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,11 +11,23 @@ namespace LojaVirtual.Models
     public class Colaborador
     {
         [Key]
+        [Display(Name ="Código")]
         public int Id { get; set; }
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_Erro001")]
+        [MinLength(4, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_Erro002")]
         public string Nome { get; set; }
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_Erro001")]
+        [EmailAddress(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_Erro004")]
         public string Email { get; set; }
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_Erro001")]
+        [MinLength(6, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_Erro002")]
         public string Senha { get; set; }
+        [NotMapped]
+        public string ConfirmaSenha { get; set; }
+
         //Tipo C = Comum e tipo G = Gerente
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_Erro001")]
         public string Tipo { get; set; }
     }
 }
